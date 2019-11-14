82cd841e70b01150536d8afab0f1800b785aac51
starsol89 <sinabro1143@gmail.com>
2019-11-14 17:43:45 +0900

    Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,14 @@
 import routes from "../routes";
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -17,7 +25,7 @@ export const postUpload = (req, res) => {
   const {
     body: { file, title, description }
   } = req;
-  // To Do: Upload and save video(비디오 업로드 및 저장)
+  // To Do: Upload and save video
   res.redirect(routes.videoDetail(324393));
 };
 
