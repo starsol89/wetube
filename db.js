56d1fb9c4b09a7ac5d48a0d16adf3ad2903d05ba
starsol89 <sinabro1143@gmail.com>
2019-12-07 14:01:13 +0900

    ESLint 적용 및 수정

--- db.js
+++ db.js
@@ -4,14 +4,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
